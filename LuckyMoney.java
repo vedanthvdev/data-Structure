62ec94df0ac3a78149dca9b17dcb3848412cba5e
VedanthVDev <vedanthvdev1996@gmail.com>
2023-01-06 19:09:18 +0530

    refined for more edge cases

--- LuckyMoney.java
+++ LuckyMoney.java
@@ -1,38 +1,46 @@
-import java.util.*;
-
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.io.*;
-import java.math.*;
-import java.lang.*;
 
 class LuckyMoney {
+
+
     public static int luckyMoneyAlgo(int money, int giftee){
         int max_luck = money/8;
         int temp = 0;
+
+        //error conditions
+        if(money<0 || money >100 || giftee<0 || giftee > 10){
+            try {
+                throw new Exception("Invalid input field \n-- > money: " + money + "\n-- > giftee: " + giftee);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return 0;
+        }
+
+        //if all the giftees can get 8
         if(max_luck>=giftee){
             return giftee;
         }
+
         else{
             int max_eights = 8 * max_luck;
             if(((giftee - max_luck) == 1 ) && ((money - max_eights)==4 || (money - max_eights)== 0) || (money-max_eights < max_luck)){
-                temp = max_luck -1;
+                temp = max_luck - 1 ;
             }else{
                 temp = max_luck;
+                int noOfPeopleThatWillBePaidfterEight = money - max_eights + max_luck;
+                //10-4 condition
+                if( giftee > noOfPeopleThatWillBePaidfterEight ){
+                    temp = 0;
+                }
             }
+            
         }
-        if (temp<0){
-            return 0;
-        }else{
-            return temp;
-        }
+        return temp;
 
     }
-// public static void main(String[] args) { 
-// LuckyMoney lucky = new LuckyMoney();
-// int soul = lucky.luckyMoneyAlgo(8,1);
-// System.out.println(soul);
-// }
+public static void main(String[] args) { 
+int soul = LuckyMoney.luckyMoneyAlgo(10,11);
+System.out.println(soul);
+}
 
 }
--- LuckyMoneyTest.java
+++ LuckyMoneyTest.java
@@ -1,21 +1,44 @@
-
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LuckyMoneyTest {
 
-    static LuckyMoney lucky = new LuckyMoney();
-
     @Test
     public void testNumberOfEightsInLuckyMoney(){
-        assertEquals(0, lucky.luckyMoneyAlgo(12, 2));
-        assertEquals(2, lucky.luckyMoneyAlgo(24, 4));
-        assertEquals(8, lucky.luckyMoneyAlgo(80, 8));
-        assertEquals(1, lucky.luckyMoneyAlgo(17, 4));
-        assertEquals(0, lucky.luckyMoneyAlgo(7, 2));
-        assertEquals(1, lucky.luckyMoneyAlgo(13, 2));
-        assertEquals(1, lucky.luckyMoneyAlgo(11, 2));
+        assertEquals(0, LuckyMoney.luckyMoneyAlgo(12, 2));
+        assertEquals(2, LuckyMoney.luckyMoneyAlgo(24, 4));
+        assertEquals(8, LuckyMoney.luckyMoneyAlgo(80, 8));
+        assertEquals(1, LuckyMoney.luckyMoneyAlgo(17, 4));
+        assertEquals(0, LuckyMoney.luckyMoneyAlgo(7, 2));
+        assertEquals(1, LuckyMoney.luckyMoneyAlgo(13, 2));
+        assertEquals(1, LuckyMoney.luckyMoneyAlgo(11, 2));
 
     }
+
+    @Test
+    public void edgeCases10_10(){
+        assertEquals(0, LuckyMoney.luckyMoneyAlgo(10, 10));
+    }
+
+    @Test
+    public void edgeCases10_3(){
+        assertEquals(1, LuckyMoney.luckyMoneyAlgo(10, 3));
+    }
+
+    @Test
+    public void edgeCases10_4(){
+        assertEquals(0, LuckyMoney.luckyMoneyAlgo(10, 4));
+    }
+
+    // @Test
+    // public void edgeCases60_18(){
+    //     assertEquals(6, LuckyMoney.luckyMoneyAlgo(60, 18));
+    // }
+
+    // @Test
+    // public void edgeCases60_19(){
+    //     assertEquals(5, LuckyMoney.luckyMoneyAlgo(60, 19));
+    // }
+
 }
